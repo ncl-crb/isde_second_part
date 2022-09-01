fb446efe9f7ae965a0851f083acaa95b8159eb24
ncl-crb <crobu95@gmail.com>
2022-09-01 09:19:55 +0200

    Until lesson 26 part 1

--- src/strategy_design_pattern/pub_3.py
+++ src/strategy_design_pattern/pub_3.py
@@ -1,53 +1,85 @@
 from abc import ABC, abstractmethod
 
+
 #  module: strategy
 class Strategy(ABC):
 
     @abstractmethod
     def add_drink(self, n_drinks, price):
-        pass
+        return n_drinks * price
 
     @abstractmethod
     def get_actual_dress(self):
         pass
 
 
-class NormalHour:
-    def add_drink(self, value):
-        return value
+class NormalHour(Strategy):
+    def add_drink(self, n_drinks, price):
+        return super().add_drink(n_drinks, price)
 
     def get_actual_dress(self):
         print('normal dress')
 
 
-class HappyHour:
-    def add_drink(self, value):
+class HappyHour(Strategy):
+    def add_drink(self, n_drinks, price):
         happy_hour_discount = 0.5
-
-        return value * happy_hour_discount
+        return super().add_drink(n_drinks, price) * happy_hour_discount
 
     def get_actual_dress(self):
         print('happy hour dress')
 
+
 #  module: customer
 class Customer:
-    def __init__(self, strategy):
+    def __init__(self, Strategy):
         self.cost = 0
-        self._strategy = strategy
+        self.strategy = Strategy
 
-    def add_drink
+    @property
+    def strategy(self):
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, value):
+        self._strategy = value
+
+    def print_bill(self):
+        print(self.cost)
+
+    def add_drink(self, n_drinks, price):
+        self.cost += self._strategy.add_drink(n_drinks, price)
+
+    def get_actual_dress(self):
+        self._strategy.get_actual_dress()
+
 
 if __name__ == '__main__':
+    # Prepare strategies
+
+    normal_strategy = NormalHour()
+    happy_hour_strategy = HappyHour()
+
     # NORMAL BILLING
-    customer1 = CustomerNormalHour()
+
+    customer1 = Customer(normal_strategy)
     customer1.add_drink(1, 7)
+
     customer1.get_actual_dress()
+
     # START HAPPY HOUR (50% discount)
-    customer1.__class__ = CustomerHappyHour
-    # You are changing class at runtime!
-    # It is possible, but are we sure it is a good idea?
+
+    customer1.strategy = happy_hour_strategy
+    customer2 = Customer(happy_hour_strategy)
+
+    customer2.add_drink(1, 7)
+
     customer1.add_drink(2, 5)
+    customer2.add_drink(2, 5)
     customer1.get_actual_dress()
+
     # FINAL BILL
     customer1.get_actual_dress()
+
     customer1.print_bill()  # 12
+    customer2.print_bill()  # 8.5
